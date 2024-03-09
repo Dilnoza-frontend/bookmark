393b70a3e79fb372039cb5f3cb133f709e109190
Dilnoza-frontend <dilnoza_matyakubova@yahoo.com>
2024-03-09 01:07:27 -0500

    Make feature tabs work

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 const elsTabsItem = document.querySelectorAll('.tabs__item');
+const elsTabsPanel = document.querySelectorAll('.tabs__panel');
 const elsTabLink = document.querySelectorAll('.js-tab-link');
+
+function deactivateTabItems () {
+  elsTabsItem.forEach(function (elTabsItem) {
+    elTabsItem.classList.remove('tabs__item--active');
+  });
+}
+
+function deactivateTabPanels () {
+  elsTabsPanel.forEach(function (elTabsPanel) {
+    elTabsPanel.classList.remove('tabs__panel--active');
+  });
+}
 
 elsTabLink.forEach(function (elTabLink) {
  elTabLink.addEventListener('click', function (evt) {
@@ -7,14 +20,18 @@ elsTabLink.forEach(function (elTabLink) {
   evt.preventDefault();
 
   //Remove active class form tabs__item elements
-  elsTabsItem.forEach(function (elTabsItem) {
-    elTabsItem.classList.remove('tabs__item--active');
-  });
+    deactivateTabItems();
 
   // Add active class to current tabs__item
   elTabLink.parentElement.classList.add('tabs__item--active');
 
-  console.log(elTabLink.href.split('#')[1]);
+// Remove active class from tabs__panel elements
+  deactivateTabPanels();
+
+// Show active tab panel
+// const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
+  const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
+  elTargetPanel.classList.add('tabs__panel--active');
 
  });
 });
